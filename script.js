e5026b5189f0def60a33dfe22400b39a6cb45993
Shanugarg499 <shanugarg499@gmail.com>
2021-04-28 23:33:29 +0530

    Google-signin added. Sign to add soon.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const logvar = document.getElementsByClassName('g-signin2')[0]
+var loggedin = false
+var x
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
@@ -7,4 +10,32 @@ toggleButton.addEventListener('click', () => {
 
 function flipToggle(x){
     x.classList.toggle("change");
+}
+
+function onSignIn(googleUser) {
+    if (loggedin === true){
+        signOut()
+    }else{
+        signIn(googleUser)
+    }
+    toggle_signin()
+}
+
+function toggle_signin(){
+    logvar.classList.toggle('flip')
+}
+
+function signIn(googleUser){
+    x = googleUser.getBasicProfile()
+    console.log(googleUser.getBasicProfile())
+    loggedin = true
+    console.log(`status : `+loggedin)
+}
+
+function signOut() {
+    gapi.auth2.getAuthInstance().signOut().then(function() {
+      console.log('user signed out')
+    })
+    loggedin = false
+    console.log(`status : `+loggedin)
 }

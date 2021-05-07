5f37c4dc5e772e33252c2a1dcc70999bfe685b92
Shanugarg499 <shanugarg499@gmail.com>
2021-05-07 22:36:30 +0530

    test

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ function showusername(){
 function savefeedback(){
     if(loggedin){
         firebase.database().ref('/feedbacks/'+ user.getName().split(' ')[0] + '_' + user.getFamilyName()).set({
-            "feedback" : document.getElementById('feedback'),
+            "feedback" : document.getElementById('feedback').value,
             "Image" : user.getImageUrl(),
             "Email" : user.getEmail()
         });
